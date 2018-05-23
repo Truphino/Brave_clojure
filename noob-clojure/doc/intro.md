43e6027522a064f456261bae3975b5cb66ab682a
Truphino <thomas.recompsat@gmail.com>
2018-05-23 12:01:57 +0200

    first commit

---
+++ noob-clojure/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to noob-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ noob-clojure/test/noob_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns noob-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [noob-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
